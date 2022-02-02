827cfeba4e8eb21b2aab090532ef7c6405735550
Abdul Qayoom Jat <78290359+NQSoftwareSolutions@users.noreply.github.com>
2022-02-02 15:57:37 +0500

    Exponentiation & modulo (Remainder) are done

---
+++ basic_syntex/comments.py
@@ -0,0 +1,20 @@
+# Todo Comments in Python
+
+# Todo One line
+"""
+    We can comment any single line of code by using hash "#"
+    eg:
+        # print("Abdul Qayoom")
+"""
+# print("Abdul Qayoom")
+# Todo multiline
+"""
+    is this multi line comment
+    Ye this is
+"""
+"""
+print("Abdul Qayoom")
+print("Abdul Qayoom")
+print("Abdul Qayoom")
+"""
+
--- basic_syntex/number_operations.py
+++ basic_syntex/number_operations.py
@@ -1,1 +1,34 @@
 # Todo Numbers - Exponentiation & Modulo
+
+# Todo Exponentiation (Power)
+"""
+    Exponentiation is a mathematical operation, written as bⁿ, involving two numbers,
+     the base b and the exponent or power n, and pronounced as "b raised to the power
+      of n".
+      In python eg:
+            a = 5
+            b = 2
+            expo = a ** b
+
+            will return expo = 25
+"""
+a = 5
+b = 2
+expo = a ** b
+print("Exponentiation : " + str(expo))
+
+# Todo Modulo operations
+"""
+    In computing, the modulo operation returns the remainder or signed remainder 
+    of a division, after one number is divided by another.
+    Given two positive numbers a and n, a modulo n is the remainder of 
+    the Euclidean division of a by n, where a is the dividend and n is the divisor.
+    eg:
+        remainder = 10 % 4
+        
+        this will return 2 b/c 10 is not fully division able with 4
+        & 8 is completely division able with 4
+        & if we do 10 - 8 it will return 2 
+"""
+remainder = 10 % 4
+print("Remainder : " + str(remainder))
