a91ecae5c49af922a175495afaa8ab09d4309de5
Abdul Qayoom Jat <78290359+NQSoftwareSolutions@users.noreply.github.com>
2022-02-02 17:34:05 +0500

    Order of precedence done

--- basic_syntex/arithmetic_order_precedence.py
+++ basic_syntex/arithmetic_order_precedence.py
@@ -1,1 +1,33 @@
 # Todo Arithmetic order of precedence
+"""
+    / & * have same ordered of precedence
+    - & + have same ordered of precedence
+    () = parentheses is used to set own order of precedence
+
+    operators with same precedence will be solved from left to right
+
+"""
+# Todo with out bracket
+# Todo just sum
+p = 5 + 5 + 5 + 10 + 10 + 10
+print("Just sum : " + str(p))
+
+# Todo just subtraction
+p = 5 - 5 - 5 - 10 - 10 - 10
+print("Just subtraction : " + str(p))
+
+# Todo just subtraction & summation
+p = 5 - 5 - 5 + 10 + 10 + 10
+print("Just sum & sub : " + str(p))
+
+# Todo just multiplication, subtraction & summation
+p = 5 - 5 - 5 * 10 + 10 + 10
+print("Just mul, sum & sub : " + str(p))
+
+# Todo just division, multiplication, subtraction & summation
+p = 5 - 5 - 5 * 10 / 10 + 10
+print("Just div, mul, sum & sub : " + str(p))
+
+# Todo effect of parentheses
+p = (5 - 5 - 5) * 10 / 10 + 10
+print("Effect of parentheses : " + str(p))
---
+++ basic_syntex/shortcuts.py
@@ -0,0 +1,42 @@
+# Todo some short cuts for doing operations in python
+
+# Todo Addition
+a = b = 10
+
+a = a + 10
+b += 10
+"""
+    both operations will return same result
+"""
+print("Sum A = " + str(a) + " & B = " + str(b))
+
+# Todo Subtraction
+a = b = 10
+
+a = a - 10
+b -= 10
+"""
+    both operations will return same result
+"""
+print("Subtraction A = " + str(a) + " & B = " + str(b))
+
+# Todo Multiplication
+a = b = 10
+
+a = a * 10
+b *= 10
+"""
+    both operations will return same result
+"""
+print("Multiplication A = " + str(a) + " & B = " + str(b))
+
+# Todo Division
+a = b = 10
+
+a = a / 10
+b /= 10
+"""
+    both operations will return same result
+"""
+print("Division A = " + str(a) + " & B = " + str(b))
+
